0465aa8b0eb4bb72b21fc65706be2855ffcf50c6
haibara502 <haibara502@gmail.com>
2017-12-19 14:54:26 -0700

    prepare the dataset

---
+++ Dataset/Supreme_Court/make_dataset.py
@@ -0,0 +1,81 @@
+import re
+import nltk
+#Already downloaded
+#nltk.download() 
+
+#The input file to read the conversations.
+convers_file = open("supreme.conversations.txt", "r")
+#The input file to read the genders.
+gender_file = open("supreme.gender.txt", "r")
+#The input file to read the names.
+#name_file = open()
+#The output file to store the dataset input
+output_x = open("supreme_input.txt", "w")
+#The output file to store the dadtaset output
+output_y = open("supreme_output.txt", "w")
+
+#Read all the people genders, create a dictionary to store the info
+people_info = dict()
+while 1:
+	line = gender_file.readline()
+	if not line:
+		break
+	info = line.split(' +++$+++ ')
+	label = -1
+	if info[1] == 'female\n':
+		label = 1
+	else :
+		if info[1] == 'male\n':
+			label = 0
+	people_info[info[0]] = label
+	print info
+	print label
+
+#Start to read the conversations and create the dataset
+utterances = []
+speakers = []
+genders = []
+while 1:
+	line = convers_file.readline()
+	if not line:
+		break;
+	contents = line.split(" +++$+++ ", 14)
+	print contents
+
+	#If this utterance is the beginning of the conversation
+	new_conversation = contents[2]
+	if new_conversation == "FALSE":
+		utterances = []
+		speakers = []
+		genders = []
+
+	content = contents[-1]
+	wordtokens = nltk.tokenize.word_tokenize(content)
+	final = ' '.join(wordtokens)
+	utterances.append(final)
+	print final
+
+	speaker = contents[3]
+	speakers.append(speaker)
+	print speaker
+
+	gender = people_info[speaker]
+	genders.append(gender)
+	print gender
+
+	total_utterance = len(utterances)
+	print total_utterance
+	print utterances
+	if total_utterance <= 1:
+		continue
+
+	output_x.write(str(total_utterance - 1) + '\n')
+	for i in range(1, total_utterance):
+		if genders[i] == -1:
+			continue
+		output_x.write(speakers[i - 1] + '\n')
+		output_x.write(utterances[i - 1] + '\n')
+		output_y.write(str(genders[i]) + '\n')
+
+#	break
+
