66f7a7364b0fe54fe6a35c168d8c3c4787e9c275
haibara502 <haibara502@gmail.com>
2017-12-20 15:19:51 -0700

    Network

--- Model/network.py
+++ Model/network.py
@@ -1,14 +1,25 @@
 import numpy as np
+import keras
 from scipy.sparse import csr_matrix
 from keras.preprocessing.text import Tokenizer
 from keras.utils import to_categorical
 from keras.layers import Dense, Input
+from keras.layers import TimeDistributed
 from keras.layers import Embedding
+from keras.layers import Bidirectional, LSTM, Concatenate
 from keras.models import Model
 import string
 
 WORD_VECTOR_FILE = '../Dataset/Supreme_Court/vector.txt'
 EMBEDDING_DIM = 100
+VALIDATION_SPLIT = 0.9
+NUMBER_OF_UTTERANCE = None
+SENTENCE_LENGTH = 145
+WORD_NUM = 60
+WORD_EMBEDDING_SIZE = 100
+NUM_HIDDEN = 50
+NUMBER_OF_NAMES = 327
+NAME_EMBEDDING_SIZE = 100
 
 print('Load pretrained word vectors.')
 
@@ -35,7 +46,7 @@ word_vector_file.close()
 
 print('Start to read the text.')
 
-TEXT_INPUT_FILE = '../Dataset/Supreme_Court/supreme_input.txt'
+TEXT_INPUT_FILE = '../Dataset/Supreme_Court/supreme_input_temp.txt'
 LABEL_INPUT_FILE = '../Dataset/Supreme_Court/supreme_output.txt'
 text_input_file = open(TEXT_INPUT_FILE, "r")
 label_input_file = open(LABEL_INPUT_FILE, "r")
@@ -48,8 +59,7 @@ MAX_WORDS = 10788
 tokenizer = Tokenizer(num_words=MAX_WORDS)
 tokenizer.fit_on_texts(texts)
 
-MAX_NAME = 311
-name_tokenizer = Tokenizer(num_words = MAX_NAME)
+MAX_NAME = 326
 
 all_names = dict()
 
@@ -85,6 +95,7 @@ while 1:
 			all_names[name] = len(all_names)
 			name_id = all_names.get(name)
 			name_one_script.append(name_id)
+
 		label_index = int(label_input_file.readline())
 		label.append(label_index)	
 
@@ -94,7 +105,6 @@ while 1:
 
 MAX_LABEL = 2
 
-name_tokenizer.fit_on_texts(all_names)
 names_temp = []
 for item in names:
 	temp_item = np.array(item)
@@ -109,7 +119,7 @@ print("Shuffle the data")
 print len(scripts)
 print len(names)
 print len(labels)
-exit()
+
 scripts = np.array(scripts)
 indices = np.arange(scripts.shape[0])
 np.random.shuffle(indices)
@@ -127,17 +137,15 @@ for i in range(names.shape[0]):
 	names_temp.append(names[indices[i]])
 names = np.array(names_temp)
 
-print scripts.shape
-print names.shape
-print labels.shape
-exit()
 labels_temp = []
 labels = np.array(labels)
 for i in range(labels.shape[0]):
 	labels_temp.append(labels[indices[i]])
 labels = np.array(labels_temp)
-print labels
-exit()
+
+print labels.shape
+print scripts.shape
+print names.shape
 
 num_validation_samples = int(VALIDATION_SPLIT * scripts.shape[0])
 
@@ -149,24 +157,23 @@ labels_train = labels[:-num_validation_samples]
 labels_valid = labels[-num_validation_samples]
 
 script_input = Input(shape=(NUMBER_OF_UTTERANCE, SENTENCE_LENGTH, WORD_NUM))
-script_embedding = TimeDistributed(
-	Embedding(WORD_NUM, output_dim=WORD_EMBEDDING_SIZE, weights=[embedding_matrix], input_length=MAX_SEQUENCE_LENGTH,
-			  trainable=True)(script_input))
-script_output = Bidirectional(LSTM(NUM_HIDDEN, return_sequences=True), merge_mode='concat')(script_embedding)
+script_output = TimeDistributed(Bidirectional(LSTM(NUM_HIDDEN, return_sequences=True), merge_mode='concat'))(script_input)
 
 model = Model(inputs=script_input, outputs=script_output)
 
 print(model.input_shape)
 print(model.output_shape)
 
-name_input = Input(shape=(NUMBER_OF_UTTERANCE,))
-name_output = Embedding(NUMBER_OF_NAMES, output_dim=NAME_EMBEDDING_SIZE)(name_input)
+name_input = Input(shape=(NUMBER_OF_UTTERANCE, ))
 
+#name_output = Embedding(input_length = NUMBER_OF_UTTERANCE, input_dim = NUMBER_OF_NAMES, output_dim=NAME_EMBEDDING_SIZE)(name_input)
+
+name_output = TimeDistributed(Dense(EMBEDDING_DIM))(name_input)
 model = Model(inputs=name_input, outputs=name_output)
 print(model.input_shape)
 print(model.output_shape)
 
-utterance_input = Concatenate(axis=-1)([script_output, name_output])
+utterance_input = Concatenate(axis = -1)([script_output, name_output])
 
 output = LSTM(SCRIPT_EMBEDDING_SIZE)(utterance_input)
 output = Dense(LABEL_SIZE, activation='softmax')(output)
